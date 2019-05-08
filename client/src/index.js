602825f6d71f175305e47c851a200438f316909b
Sergio Pérez <sergi9p@gmail.com>
2019-05-08 22:56:57 +0200

    Global gitignore + remove bootstrap import on index to fix modal

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 
 ReactDOM.render(
