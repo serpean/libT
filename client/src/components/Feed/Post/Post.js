e6b4a3b9e3794b524e54142700cedc0218cc6c65
Sergio Pérez <sergi9p@gmail.com>
2019-05-13 22:44:14 +0200

    First post representation

--- client/src/components/Feed/Post/Post.js
+++ client/src/components/Feed/Post/Post.js
@@ -1,29 +1,33 @@
 import React from 'react';
 
 import Button from '../../Button/Button';
+import Image from '../../Image/Image';
 import './Post.css';
+
+const action = ['quiere', 'está', 'terminó'];
+const actionType = {
+  book: ['leer', 'leyendo', 'de leer'],
+  movie: ['ver', 'viendo', 'de ver'],
+  game: ['jugar', 'jugando', 'de jugar']
+};
 
 const post = props => (
   <article className="post">
     <header className="post__header">
       <h3 className="post__meta">
-        Posted by {props.author} on {props.date}
+        {props.author} {action[props.action]}{' '}
+        {actionType[props.type] ? actionType[props.type][props.action] : ''} el{' '}
+        {props.date}
       </h3>
       <h1 className="post__title">{props.title}</h1>
     </header>
-    {/* <div className="post__image">
+    <div className="post__image">
       <Image imageUrl={props.image} contain />
     </div>
-    <div className="post__content">{props.content}</div> */}
+    <div className="post__content">{props.content}</div>
     <div className="post__actions">
       <Button mode="flat" link={props.id}>
         View
-      </Button>
-      <Button mode="flat" onClick={props.onStartEdit}>
-        Edit
-      </Button>
-      <Button mode="flat" design="danger" onClick={props.onDelete}>
-        Delete
       </Button>
     </div>
   </article>
