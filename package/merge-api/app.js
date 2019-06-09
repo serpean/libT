1988a23fce555b81db3457fadd463d7508b765e3
Sergio Pérez <sergi9p@gmail.com>
2019-06-09 11:01:37 +0200

    Add prueba

--- package/merge-api/app.js
+++ package/merge-api/app.js
@@ -17,7 +17,7 @@ app.get("/:type/:id", async (req, res, next) => {
     const factory = new RequestStrategyFactory();
     const requestStrategy = factory.createRequestStrategy(type);
     const result = await requestStrategy.doRequest(id);
-    res.json(result);
+    res.status(200).json(result);
   } catch (err) {
     if (!err.statusCode) err.statusCode = 500;
     next(err);
@@ -37,12 +37,17 @@ app.get("/", async (req, res, next) => {
       type: req.query.type,
       page: page
     });
-    res.json(result);
+    res.status(200).json(result);
   } catch (err) {
     if (!err.statusCode) err.statusCode = 500;
     next(err);
   }
 });
+
+app.get("/prueba", async (req, res, next) => {
+  res.status(200).json({message: "Hola"});
+  }
+);
 
 app.use((err, req, res, next) => {
   console.log(err);
